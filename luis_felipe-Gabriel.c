f6b374eace012e32c3d5c727c29ec8bd9b8658f0
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2022-06-11 09:54:20 -0300

    Adicionar Menus

--- luis_felipe-Gabriel.c
+++ luis_felipe-Gabriel.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void adicionarCandidato()
+{
+    printf("\nA Fazer - Candidato");
+}
+
+void votar()
+{
+    printf("\nA Fazer - votar");
+}
+
+void relatorio()
+{
+    printf("\nA Fazer - relatorio");
+}
+
+void menu()
+{
+    int seletor;
+    printf("\n|| 1 - Adicionar Candidadtos || 2 - Votar || Outro - Sair e mostrar relatorio ||\n");
+    scanf("%d", &seletor);
+    switch (seletor)
+    {
+    case 1:
+        adicionarCandidato();
+        menu();
+        break;
+
+    case 2:
+        votar();
+        menu();
+        break;
+
+    default:
+        relatorio();
+        break;
+    }
+}
+
+int main()
+{
+    menu();
 
     return 0;
 }
